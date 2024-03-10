0215d7f2b251c85de28b484ad99d94325fc80ffd
root <root@DESKTOP-BLPH810>
2024-03-10 03:12:04 +0200

    sorting algorithem

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,32 @@
 #include "sort.h"
+
 /**
  * bubble_sort - function that sorts an array
  * of integers in ascending order using the
  * Bubble sort algorithm
  *
- * @array: input arrray
+ * @array: input array
  * @size: size of the array
  * Return: no return
  */
 void bubble_sort(int *array, size_t size)
 {
-    for (int i = 0; i<size-1; i++)
-    {
-        for (int j = 0; j<size-i-1; j++)
-        {
-            if (array[j]> array[j+1])
-            {
-                int temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-                print_array(array, size);
-            }
-            
-        }
-    }
+	int i;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		int j;
+
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
--- sort.h
+++ sort.h
@@ -16,7 +16,29 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+#include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
