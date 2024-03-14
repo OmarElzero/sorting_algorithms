9022fdd242a862b2186be6969c63589307ff9141
root <root@DESKTOP-BLPH810>
2024-03-14 02:30:58 +0200

    sorting algorithem -bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,27 +5,27 @@
  * of integers in ascending order using the
  * Bubble sort algorithm
  *
- * @array: input arrray
+ * @array: input array
  * @size: size of the array
  * Return: no return
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, n;
-	int tmp, swap;
+	size_t i;
 
-	for (n = size, swap = 1; n > 0 && swap; n--)
+	for (i = 0; i < size - 1; i++)
 	{
-		swap = 0;
-		for (i = 0; (i + 1) < n; i++)
+		size_t j;
+
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				tmp = array[i + 1];
-				array[i + 1] = array[i];
-				array[i] = tmp;
+				int temp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
-				swap = 1;
 			}
 		}
 	}
