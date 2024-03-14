0d5252ba48a62938adaff7378423e90cd514fa94
root <root@DESKTOP-BLPH810>
2024-03-14 04:44:55 +0200

    sorting algorithem -bubble sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in
+ * ascending order using the Selection sort
+ * algorithm
+ *
+ * @array: input array of integers
+ * @size: size of the array
+ * Return: no return
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+		if (array[j] < array[min])
+		{
+		min = j;
+		}
+
+		}
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
+	}
+}
